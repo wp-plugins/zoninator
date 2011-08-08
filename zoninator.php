e1a897af3f09ccd9e4ea9144563a93cd92d3e67b
batmoo <batmoo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-08 23:37:37 +0000

    Better way to build the URI
    git-svn-id: https://plugins.svn.wordpress.org/zoninator/trunk@420737 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- zoninator.php
+++ zoninator.php
@@ -32,7 +32,7 @@ if( ! class_exists( 'Zoninator' ) ) :
 
 define( 'ZONINATOR_VERSION', '0.1' );
 define( 'ZONINATOR_PATH', dirname( __FILE__ ) );
-define( 'ZONINATOR_URL', plugins_url( plugin_basename( dirname( __FILE__ ) ) . '/' ) );
+define( 'ZONINATOR_URL', trailingslashit( plugins_url( '', __FILE__ ) ) );
 
 require_once( ZONINATOR_PATH . '/functions.php' );
 require_once( ZONINATOR_PATH . '/widget.zone-posts.php');
